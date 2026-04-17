a95c18d52abeb02c219ba0e6f4be7a7ead1307f3
Darragh Nugent <darraghnugent312@gmail.com>
2026-04-18 01:37:46 +1000

    Created about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,92 @@
 import React from "react";
 
+import { Box, Rating, Accordion, Stack, Typography, Card, CardContent, AccordionSummary, AccordionDetails, Button, TextField, Select, MenuItem, Grid, Container } from "@mui/material";
+
+import AboutImg from "../assets/about.jpg"
+
 export default function About() {
-  return <h2>About</h2>;
+  return (
+    <Box sx={{ bgcolor: 'custom.backgroundLight', minHeight: '100vh', py: 4 }}>
+      <Container maxWidth="md">
+          <Typography variant="h4" sx={{ mb: 3, fontWeight: 600 }}>
+            About Us
+          </Typography>
+
+        <Box
+          component="img"
+          src={AboutImg}
+          alt="Login"
+          sx={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            borderRadius: 3,
+          }}
+        >
+        </Box>
+
+        <Card sx={{
+          borderRadius: 3,
+          boxShadow: 3,
+          bgcolor: 'custom.card',
+        }}>
+          <CardContent>
+            <Typography variant="body1" color="text.primary">
+              For over 45 months, High Rise has been a staple in the rental market scene. Rising like a phoenix from the ashes of Rentals-R-us
+              (for legal reasons we have no formal relation to Rentals-R-Us nor to anyone currently involved in the investigation into Rentals-R-Us),
+              High Rise has grown suspiciously quickly become a key player in properties across the country.
+              <br /><br />
+              We deal in all rentals, from multi-storey apartments, to shacks you can't quite stand upright in. Our wide range and diverse location will suit anyone's needs.
+              New families, old families, older families who are still figuring out the will can all find a new home to sit down, relax, and argue over legal documents.
+              <br /><br />
+
+              Don't believe us? here is what some of our clients had to say:
+              <br /><br />
+            </Typography>
+
+            <Box>
+              <Typography variant="body1" color="text.secondary" sx={{ fontStyle: 'italic' }}>
+                “The property was pretty good. Cosy. At least until the rats finally caved in the roof!”
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', textAlign: 'right' }}>
+                - Ian, Previously in Loganhome
+              </Typography>
+            </Box>
+
+            <Box>
+              <Typography variant="body1" color="text.secondary" sx={{ fontStyle: 'italic' }}>
+                “Can't wait until I get another rental with them. As in, I have a week left where I currently am before I am out on the street”
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', textAlign: 'right' }}>
+                - Sarah, Previously in Carlton, now in [unknown]
+              </Typography>
+            </Box>
+
+            <Box>
+              <Typography variant="body1" color="text.secondary" sx={{ fontStyle: 'italic' }}>
+                “High Rise, I thought I was with Rentals-R-Us?”
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', textAlign: 'right' }}>
+                - Greg, Currently at St Leonards
+              </Typography>
+            </Box>
+
+            <Box>
+              <Typography variant="body1" color="text.secondary" sx={{ fontStyle: 'italic' }}>
+                “Yeah, nah”
+              </Typography>
+              <Typography variant="body2" color="text.secondary" sx={{ fontStyle: 'italic', textAlign: 'right' }}>
+                - Anonymous
+              </Typography>
+            </Box>
+
+            <Typography variant="body1" color="text.primary">
+              <br />
+              So, what are you waiting for? Come find your dream home for the next few weeks today!
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
+    </Box>
+  );
 }
